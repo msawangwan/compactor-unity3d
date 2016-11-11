4c7dd5ed6c863222ad0be576a0c1f5ab9a764508
msawangwan <misha.sawangwan@gmail.com>
2016-11-10 17:35:13 -0800

    formatting fixed

--- Assets/Script/Core/Client/UI/ObjectModel/LevelTimer.cs
+++ Assets/Script/Core/Client/UI/ObjectModel/LevelTimer.cs
@@ -4,28 +4,28 @@ using UnityEngine.UI;
 namespace mUnityFramework.UI.ObjectModel {
 	public class LevelTimer : MonoBehaviour {
 		private Text cachedTimerTextField;
-        private float startTimeOffset;
+		private float startTimeOffset;
 
-        private Text timerTextField {
+		private Text timerTextField {
 			get {
 				if (! cachedTimerTextField) {
 					cachedTimerTextField = GetComponent<Text> ();
 				}
-                return cachedTimerTextField;
-            }
+				return cachedTimerTextField;
+			}
 		}
 
 		public void SetStartOffset () {
-            startTimeOffset = Time.timeSinceLevelLoad;
-        }
+			startTimeOffset = Time.timeSinceLevelLoad;
+		}
 
-        public void PrintTime() {
-            timerTextField.text = (Time.timeSinceLevelLoad - startTimeOffset).ToString ("0:00");
+		public void PrintTime() {
+			timerTextField.text = (Time.timeSinceLevelLoad - startTimeOffset).ToString ("0:00");
 		}
 
 		public void Reset () {
-            startTimeOffset = 0.0f;
-            timerTextField.text = (0.0f).ToString("0:00");
-        }
+			startTimeOffset = 0.0f;
+			timerTextField.text = (0.0f).ToString("0:00");
+		}
 	}
 }
